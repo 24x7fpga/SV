e430d30c94fa8f7d1c2b02bdcb00eb3618f14dc5
24x7fpga <kiran.24x7fpga@gmail.com>
2024-10-04 16:00:31 -0400

    static cast example

---
+++ sv_verification/static_conv/tb_static_conv.sv
@@ -0,0 +1,20 @@
+module tb_static_conv; 
+ 
+  int  i = 75;
+  real r = 13.75;
+  
+  
+  initial begin
+    
+    int  m = i * (int '(r));
+    real k = (real '(i)) * r;
+    
+    $display("multiplication result in int  = %0f", m);
+    $display("multiplication result in real = %0f", k);
+    
+  end
+  
+endmodule 
+// Local Variables: 
+// verilog-library-directories:("~/Projects/fpgaProjects/Verification/sv_verification/static_conv/*") 
+// End:
