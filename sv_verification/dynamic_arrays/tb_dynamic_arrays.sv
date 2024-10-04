7a99ab70ef705e2be35660724ea10bd077e48dc2
24x7fpga <kiran.24x7fpga@gmail.com>
2024-10-04 15:57:00 -0400

    dynamic array example

---
+++ sv_verification/dynamic_arrays/tb_dynamic_arrays.sv
@@ -0,0 +1,28 @@
+module tb_dynamic_arrays; 
+
+  int dyn1[], dyn2[];
+  
+  initial begin
+    dyn1 = new[10];
+    foreach(dyn1[i]) dyn1[i] = i;
+      
+    $display("--------------  Dynamic Array 1   --------------");
+    foreach(dyn1[i]) $display("\t dyn1[%0d] = %0d", i, dyn1[i]);
+    
+    dyn2 = dyn1;    
+    $display("--------------  Dynamic Array 2  --------------");
+    foreach(dyn2[i]) $display("\t dyn2[%0d] = %0d", i, dyn2[i]);
+
+    foreach(dyn2[i]) dyn2[i] = dyn2[i] + 5;
+    $display("--------------  Dynamic Array 2  --------------");
+    foreach(dyn2[i]) $display("\t dyn2[%0d] = %0d", i, dyn2[i]);
+    
+    dyn2 = new[15] (dyn1);
+    $display("--------------  Dynamic Array 2  --------------");
+    foreach(dyn2[i]) $display("\t dyn2[%0d] = %0d", i, dyn2[i]);
+    
+  end
+endmodule 
+// Local Variables: 
+// verilog-library-directories:("~/Projects/fpgaProjects/Verification/sv_verification/dynamic_arrays/*") 
+// End:
