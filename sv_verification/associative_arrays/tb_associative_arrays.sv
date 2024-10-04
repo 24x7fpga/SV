31abedecc355c7bdbc6bedcc8c4a65a436075fa5
24x7fpga <kiran.24x7fpga@gmail.com>
2024-10-04 15:56:26 -0400

    associative array example

---
+++ sv_verification/associative_arrays/tb_associative_arrays.sv
@@ -0,0 +1,30 @@
+module tb_associative_arrays; 
+ 
+  byte assoc[byte];
+  
+  byte indx = 1;
+  
+  int pow_2[int] = '{0:1, 1:2, 2:4, 3:8, 4:16};
+  
+  initial begin
+    
+    do begin
+      assoc[indx] = indx;
+      indx = indx << 1;
+    end while (indx != 0);
+    
+    $display("--------------  Associative Array   --------------");
+    foreach(assoc[i]) $display("\t assoc[%0d] = %0d", i, assoc[i]);
+    
+
+    $display("--------------  Associative Array   --------------");
+    for(int i=5; i<10; i++)
+      pow_2[i] = 1 << i;
+    $display("%p", pow_2);
+    
+  end
+  
+endmodule 
+// Local Variables: 
+// verilog-library-directories:("~/Projects/fpgaProjects/Verification/sv_verification/associative_arrays/*") 
+// End:
