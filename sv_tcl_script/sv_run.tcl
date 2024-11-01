091e04ebc607dcdbeb9be3b5a05e436ec8330e98
24x7fpga <kiran.24x7fpga@gmail.com>
2024-11-01 14:47:34 -0400

    updated run script

--- sv_tcl_script/sv_run.tcl
+++ sv_tcl_script/sv_run.tcl
@@ -3,7 +3,7 @@ puts "Run Simulation TCL!"
 
 # Check if the correct number of arguments are passed
 if { [llength $argv] != 2 } {
-    puts "Usage: vivado -mode batch -source uvm_run.tcl -tclargs <project_name> <project_location>"
+    puts "Usage: vivado -mode batch -source sv_run.tcl -tclargs <project_name> <project_location>"
     exit 1
 }
 
@@ -24,7 +24,7 @@ add_files [glob $project_location/*.sv]
 set_property top tb_$project_name [get_filesets sim_1]
 set_property top_lib xil_defaultlib [get_filesets sim_1]
 
-# default simulation runtime is removed; by default it is set to 1000ns
+# default simulation runtime is removed; default sim time is set to 1000ns
 set_property -name {xsim.simulate.runtime} -value {} -objects [get_filesets sim_1]
 
 # Start Simulation
