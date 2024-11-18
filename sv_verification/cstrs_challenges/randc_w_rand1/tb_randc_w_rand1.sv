8893683fd8da775913aded65ead5835519fba7aa
24x7fpga <kiran.24x7fpga@gmail.com>
2024-11-17 21:59:43 -0500

    cyclic rand without randc1

---
+++ sv_verification/cstrs_challenges/randc_w_rand1/tb_randc_w_rand1.sv
@@ -0,0 +1,11 @@
+`include "test.sv"
+
+module tb_randc_w_rand1;
+
+   parameter N = 3; // width of the required variable
+   parameter R = 4; // repeat the process R >= 2
+  
+   test #(N, R) t1 ();
+
+endmodule // tb_randc_w_rand1   
+
